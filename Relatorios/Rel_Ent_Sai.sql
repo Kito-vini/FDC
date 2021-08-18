1bd67f88880eefc89a54732d2d50ccc91e20b416
kito-vini <marcusviva11@gmail.com>
2021-08-17 22:28:07 -0300

    correção da quantidade de entrada e data saida

--- Relatorios/Rel_Ent_Sai.sql
+++ Relatorios/Rel_Ent_Sai.sql
@@ -2,12 +2,12 @@
 
 select p.idproduto codigo,
 p.descricaoproduto nome,
-(e.qtde) entrada,
+e.qtde entrada,
 sum(s.qtde) saida,
 (e.qtde)- sum(s.qtde) saldo_de_estoque
 from produto p
 inner join itementrada e on p.idproduto = e.idproduto
 inner join itemsaida s on p.idproduto = s.idproduto
-where e.da between :data_inicial and :data_final
+where e.da between :data_inicial and :data_final and s.da between :data_inicial and :data_final
 group by p.idproduto, p.descricaoproduto, e.qtde
 order by p.descricaoproduto
