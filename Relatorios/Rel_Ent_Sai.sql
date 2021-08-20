3b372d684be0cd02670785e934f899fa536fa7f5
kito-vini <marcusviva11@gmail.com>
2021-08-19 22:08:03 -0300

    Correção do relatório com subquerys

--- Relatorios/Rel_Ent_Sai.sql
+++ Relatorios/Rel_Ent_Sai.sql
@@ -1,13 +1,18 @@
 /*Relatório de entrada e saida com saldo de estoque por periodo*/
 
 select p.idproduto codigo,
-p.descricaoproduto nome,
-(e.qtde) entrada,
-sum(s.qtde) saida,
-(e.qtde)- sum(s.qtde) saldo_de_estoque
+p.descricaoproduto,
+totent.compra as entrada,
+totsa.venda as saida,
+totent.compra - totsa.venda as saldo_de_estoque
 from produto p
-inner join itementrada e on p.idproduto = e.idproduto
-inner join itemsaida s on p.idproduto = s.idproduto
-where e.da between :data_inicial and :data_final
-group by p.idproduto, p.descricaoproduto, e.qtde
-order by p.descricaoproduto
+inner join (select sum(ent.qtde) as compra, ent.idproduto as cod from itementrada as ent
+where ent.da between :data_inicial and :data_final
+ group by ent.idproduto) as totent
+on p.idproduto = totent.cod
+inner join ( select sum(sa.qtde) as venda, sa.idproduto as cod from itemsaida as sa
+where sa.da between :data_inicial and :data_final
+ group by sa.idproduto) as totsa
+on p.idproduto = totsa.cod
+group by p.idproduto, p.descricaoproduto,totent.compra, totsa.venda
+order by p.idproduto
