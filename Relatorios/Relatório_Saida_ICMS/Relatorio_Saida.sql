1fa97c68b6e9c6734cfe37a918f09a08d469c292
Kito-vini <marcusviva11@gmail.com>
2023-09-28 16:27:01 -0300

    Inclusão dos filtros

--- Relatorios/Relatório_Saida_ICMS/Relatorio_Saida.sql
+++ Relatorios/Relatório_Saida_ICMS/Relatorio_Saida.sql
@@ -11,3 +11,6 @@ inner join saida s on nf.numeroorigem = s.numerosaida
 where s.tiposaida = '2' AND NF.status ='A' AND NF.tipolf='S' AND NF.modelo='55'
 GROUP BY 1,2,3,4,5,6
 
+--FILTROS:
+-- E.IDFILIAL = SELECT IDFILIAL, NOMEFILIAL FROM FILIAL = {@SELECT_LISTA} /*FILIAL*/
+-- NF.DATAEMISSAO = {@DATA_PERIODO} /*DATA*/
