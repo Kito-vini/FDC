44d161c1bd6a196ca7256084293e9db853d3f255
Kito-vini <marcusviva11@gmail.com>
2023-09-28 15:53:50 -0300

    Criação de relatório de saída com icms

---
+++ Relatorios/Relatório_Saida_ICMS/Relatorio_Saida.sql
@@ -0,0 +1,13 @@
+select
+    NF.dataemissao AS DATA_EMISSAO,
+    NF.datasaida AS DATA_SAIDA,
+    NF.numeronf AS NOTA,
+    NF.destinatario_razaosocial AS DESTINATARIO,
+    NF.valortotalnf AS VALOR_NF,
+    NF.baseicms AS BASE_CALCULO,
+    SUM(NF.valoricms + NF.FCPVALOR) AS ICMS
+from lfnf nf
+inner join saida s on nf.numeroorigem = s.numerosaida
+where s.tiposaida = '2' AND NF.status ='A' AND NF.tipolf='S' AND NF.modelo='55'
+GROUP BY 1,2,3,4,5,6
+
